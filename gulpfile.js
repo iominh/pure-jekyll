825a2ddd8e7c055e3381a53b5a6975357473a29c
minh nguyen <minh.9.nguyen@gmail.com>
2015-04-13 19:33:58 -0400

    fix gulp sass watch and make layout single column

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,7 @@ gulp.task('sass', function () {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('_scss/*.scss', ['sass']);
+    gulp.watch('_scss/**/*.scss', ['sass']);
     gulp.watch(['index.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
